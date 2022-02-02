19dc5893b9878175007da5ff82933008da3022cf
Nikita Cunskis <nikita.cunskis@gmail.com>
2022-02-02 19:16:36 +0200

    init commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,46 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+      <div v-bind:title="beers[$route.params.id - 1].tagline">
+          <img v-bind:src="beers[$route.params.id - 1].image_url" width="288"/>
+          <h3>{{ beers[$route.params.id - 1].name }}</h3>
+          <p>{{ beers[$route.params.id - 1].description }}</p>
+          <ul>
+            <li><b>Ph level:</b><br> {{ beers[$route.params.id - 1].ph }}</li>
+            <li><b>Food Pairing:</b><br></li>
+            <ul>
+              <li v-for="food in beers[$route.params.id - 1].food_pairing">
+                {{ food }}
+              </li>
+            </ul>
+            <li><b>Brewer tips:</b><br>{{ beers[$route.params.id - 1].brewers_tips }}</li>
+            <li><b>Fermentation temp:</b><br>{{ beers[$route.params.id - 1].method.fermentation.temp.value }} {{ beers[$route.params.id - 1].method.fermentation.temp.unit }}</li>
+            <li><b>Mash temp:</b><br></li>
+            <ul>
+              <li v-for="mash_temp in beers[$route.params.id - 1].method.mash_temp">
+                <i>Temp:</i>
+                {{mash_temp.temp.value}} {{mash_temp.temp.unit}}
+                <i>Duration:</i>
+                {{mash_temp.duration}}
+              </li>
+            </ul>
+          </ul>
+      </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      beers: []
+    }
+  },
+  created() {
+    fetch('https://api.punkapi.com/v2/beers')
+        .then(response => response.json())
+        .then(json => {
+          this.beers = json
+        });
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="cards">
+      <a class="card" v-for="beer in beers">
+        <div v-bind:title="beer.tagline">
+          <div class="card-hero" >
+            <img v-bind:src="beer.image_url" width="288"/>
+          </div>
+          <div class="card-header">
+            <h3>{{ beer.name }}</h3>
+          </div>
+          <div class="card-body">
+            <p>{{ beer.description }}</p>
+          </div>
+        </div>
+        <router-link :to="{name: 'About', params: {id: beer.id}}">More</router-link>
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      beers: [],
+      filter:{
+        maxYear: false
+      }
+    }
+  },
+  created() {
+    fetch('https://api.punkapi.com/v2/beers')
+        .then(response => response.json())
+        .then(json => {
+          this.beers = json
+        });
   }
 }
 </script>
